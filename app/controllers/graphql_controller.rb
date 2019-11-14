0f02c52382c0191ed801e44a001e96f573f53348
ekaterina <ekaterina.v.nikonova@gmail.com>
2019-11-14 19:14:29 +0100

    Add todo type, all todos query; include authentication (current_user) in GQL controller

--- app/controllers/graphql_controller.rb
+++ app/controllers/graphql_controller.rb
@@ -4,13 +4,15 @@ class GraphqlController < ApplicationController
   # but you'll have to authenticate your user separately
   # protect_from_forgery with: :null_session
 
+  before_action :authorize_access_request!
+
   def execute
     variables = ensure_hash(params[:variables])
     query = params[:query]
     operation_name = params[:operationName]
     context = {
       # Query context goes here, for example:
-      # current_user: current_user,
+      current_user: current_user
     }
     result = GqlCraSchema.execute(query, variables: variables, context: context, operation_name: operation_name)
     render json: result
--- app/graphql/types/query_type.rb
+++ app/graphql/types/query_type.rb
@@ -3,11 +3,9 @@ module Types
     # Add root-level fields here.
     # They will be entry points for queries on your schema.
 
-    # TODO: remove me
-    field :test_field, String, null: false,
-      description: "An example field added by the generator"
-    def test_field
-      "Hello World!"
+    field :all_todos, [TodoType], null: false
+    def all_todos
+      context[:current_user].todos
     end
   end
 end
